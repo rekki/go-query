f676b4e812e6d071fb9dae8b22213d96622433a5
borislav nikolov <jack@sofialondonmoskva.com>
2019-11-30 12:22:52 +0100

    space

--- util/README.md
+++ util/README.md
@@ -2,38 +2,36 @@
 --
 Simlpe utils to tokenize and normalize text
 
-Example: package main
+Example:
 
-import (
+    package main
 
-    "fmt"
+    import (
+    	"fmt"
 
-    n "github.com/jackdoe/go-query/util/norm"
-    t "github.com/jackdoe/go-query/util/tokenize"
-
-)
-
-func main() {
-
-    tokenizer := []t.Tokenizer{
-    	t.NewWhitespace(),
-    	t.NewLeftEdge(1),
-    	t.NewUnique(),
-    }
-    normalizer := []n.Normalizer{
-    	n.NewUnaccent(),
-    	n.NewLowerCase(),
-    	n.NewSpaceBetweenDigits(),
-    	n.NewCleanup(n.BASIC_NON_ALPHANUMERIC),
-    	n.NewTrim(" "),
-    }
-
-    tokens := t.Tokenize(
-    	n.Normalize("Hęllö World yęar2019 ", normalizer...),
-    	tokenizer...,
+    	n "github.com/jackdoe/go-query/util/norm"
+    	t "github.com/jackdoe/go-query/util/tokenize"
     )
 
-    fmt.Printf("%v", tokens)
-    // prints [h he hel hell hello w wo wor worl world y ye yea year 2 20 201 2019]
+    func main() {
+    	tokenizer := []t.Tokenizer{
+    		t.NewWhitespace(),
+    		t.NewLeftEdge(1),
+    		t.NewUnique(),
+    	}
+    	normalizer := []n.Normalizer{
+    		n.NewUnaccent(),
+    		n.NewLowerCase(),
+    		n.NewSpaceBetweenDigits(),
+    		n.NewCleanup(n.BASIC_NON_ALPHANUMERIC),
+    		n.NewTrim(" "),
+    	}
 
-}
+    	tokens := t.Tokenize(
+    		n.Normalize("Hęllö World yęar2019 ", normalizer...),
+    		tokenizer...,
+    	)
+
+    	fmt.Printf("%v", tokens)
+    	// prints [h he hel hell hello w wo wor worl world y ye yea year 2 20 201 2019]
+    }
--- util/util.go
+++ util/util.go
@@ -1,36 +1,36 @@
 // Simlpe utils to tokenize and normalize text
 //
 // Example:
-// package main
+//  package main
 //
-// import (
-// 	"fmt"
+//  import (
+//  	"fmt"
 //
-// 	n "github.com/jackdoe/go-query/util/norm"
-// 	t "github.com/jackdoe/go-query/util/tokenize"
-// )
+//  	n "github.com/jackdoe/go-query/util/norm"
+//  	t "github.com/jackdoe/go-query/util/tokenize"
+//  )
 //
-// func main() {
-// 	tokenizer := []t.Tokenizer{
-// 		t.NewWhitespace(),
-// 		t.NewLeftEdge(1),
-// 		t.NewUnique(),
-// 	}
-// 	normalizer := []n.Normalizer{
-// 		n.NewUnaccent(),
-// 		n.NewLowerCase(),
-// 		n.NewSpaceBetweenDigits(),
-// 		n.NewCleanup(n.BASIC_NON_ALPHANUMERIC),
-// 		n.NewTrim(" "),
-// 	}
+//  func main() {
+//  	tokenizer := []t.Tokenizer{
+//  		t.NewWhitespace(),
+//  		t.NewLeftEdge(1),
+//  		t.NewUnique(),
+//  	}
+//  	normalizer := []n.Normalizer{
+//  		n.NewUnaccent(),
+//  		n.NewLowerCase(),
+//  		n.NewSpaceBetweenDigits(),
+//  		n.NewCleanup(n.BASIC_NON_ALPHANUMERIC),
+//  		n.NewTrim(" "),
+//  	}
 //
-// 	tokens := t.Tokenize(
-// 		n.Normalize("Hęllö World yęar2019 ", normalizer...),
-// 		tokenizer...,
-// 	)
+//  	tokens := t.Tokenize(
+//  		n.Normalize("Hęllö World yęar2019 ", normalizer...),
+//  		tokenizer...,
+//  	)
 //
-// 	fmt.Printf("%v", tokens)
-// 	// prints [h he hel hell hello w wo wor worl world y ye yea year 2 20 201 2019]
-// }
+//  	fmt.Printf("%v", tokens)
+//  	// prints [h he hel hell hello w wo wor worl world y ye yea year 2 20 201 2019]
+//  }
 //
 package util
